fd642e7b26b89e9acbee26993716fef0698ab1c1
sargisheiser <sargis.heiser@gmail.com>
2025-10-02 21:54:50 +0200

    Added data manager with CRUD

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-from models import db, User, Movie
+from models import db
+from data_manager import DataManager
 import os
 
 app = Flask(__name__)
@@ -9,17 +10,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+dm = DataManager()
+
 
 @app.route("/")
 def index():
-    users = User.query.all()  # get all users from DB
+    users = dm.get_users()
     return render_template("index.html", users=users)
 
 
 @app.route("/movies/<int:user_id>")
 def movies(user_id):
-    user = User.query.get_or_404(user_id)
-    return render_template("movies.html", username=user.name, movies=user.movies)
+    movies = dm.get_movies(user_id)
+    user = None
+    if movies:
+        user = movies[0].user
+    else:
+        from models import User
+        user = User.query.get(user_id)
+    return render_template("movies.html", username=user.name, movies=movies)
 
 
 if __name__ == "__main__":
---
+++ data_manager.py
@@ -0,0 +1,49 @@
+from models import db, User, Movie
+
+
+class DataManager:
+
+    def create_user(self, name):
+        """Add a new user to the DB."""
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
+
+    def get_users(self):
+        """Return a list of all users."""
+        return User.query.all()
+
+    def get_movies(self, user_id):
+        """Return all movies for a specific user."""
+        user = User.query.get(user_id)
+        return user.movies if user else []
+
+    def add_movie(self, name, director, year, poster_url, user_id):
+        """Add a new movie to a user's favorites."""
+        new_movie = Movie(
+            name=name,
+            director=director,
+            year=year,
+            poster_url=poster_url,
+            user_id=user_id
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return new_movie
+
+    def update_movie(self, movie_id, new_title):
+        """Update the title of a movie."""
+        movie = Movie.query.get(movie_id)
+        if movie:
+            movie.name = new_title
+            db.session.commit()
+        return movie
+
+    def delete_movie(self, movie_id):
+        """Delete a movie by ID."""
+        movie = Movie.query.get(movie_id)
+        if movie:
+            db.session.delete(movie)
+            db.session.commit()
+        return movie
